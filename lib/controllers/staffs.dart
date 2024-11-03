b123d2ca80d05f7a744fad2de875a0f06ddbb2ed
Joyfy Tech <joyfytech@gmail.com>
2024-11-03 19:40:50 +0530

    update watsapp code

--- lib/controllers/staffs.dart
+++ lib/controllers/staffs.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Staffs extends GetxController {
   // Controllers for text fields
@@ -50,7 +51,26 @@ class Staffs extends GetxController {
         'createdAt': FieldValue.serverTimestamp(), // Store as Timestamp
       };
 
-      await FirebaseFirestore.instance.collection('staffs').add(staffData);
+      // Create a new document with an auto-generated ID
+      DocumentReference docRef =
+          await FirebaseFirestore.instance.collection('staffs').add(staffData);
+      String uniqueId = docRef.id;
+
+      // Update the document to include the ID within the document's data
+      await FirebaseFirestore.instance
+          .collection('staffs')
+          .doc(uniqueId)
+          .update({
+        'id': uniqueId,
+      });
+
+      // Send WhatsApp message after adding staff
+      await _sendWhatsAppMessage(
+        mobileController.text,
+        uniqueId,
+        passwordController.text,
+      );
+
       clearFields(); // Clear fields after adding
       Get.snackbar('Success', 'Staff added successfully');
     }
@@ -79,6 +99,7 @@ class Staffs extends GetxController {
           .collection('staffs')
           .doc(id)
           .update(staffData);
+
       clearFields(); // Clear fields after updating
       Get.snackbar('Success', 'Staff updated successfully');
     }
@@ -110,5 +131,24 @@ class Staffs extends GetxController {
   Future<void> fetchStaff() async {
     var snapshot = await FirebaseFirestore.instance.collection('staffs').get();
     staffList.assignAll(snapshot.docs);
+  }
+
+  // Send WhatsApp message
+  Future<void> _sendWhatsAppMessage(
+      String phoneNumber, String memberID, String memberPassword) async {
+    final String message = 'Hello $nameController.text,\n\n'
+        'Welcome to JK EVENT MANAGEMENT! \n\n'
+        'This is your MemberID and MemberPassword. \n\n'
+        'Member ID: $memberID\n'
+        'Member Password: $memberPassword\n\n'
+        'Please do not share your MemberID and MemberPassword with anyone. \n\n';
+
+    final Uri url = Uri.parse(
+        'https://wa.me/+91$phoneNumber?text=${Uri.encodeComponent(message)}');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
